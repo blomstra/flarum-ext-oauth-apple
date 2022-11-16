3e6095de87dcd4c2241d63d3c64984dceab2c4f2
Ian Morland <ian@morland.me>
2022-11-16 21:36:36 +0000

    chore: use shared oauth settings page

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,9 +1,9 @@
 import app from 'flarum/admin/app';
-import AppleOAuthPage from './components/AppleOAuthSettingsPage';
+import { ConfigureWithOAuthPage } from '@fof-oauth';
 import ExtendOAuthSettings from './ExtendOAuthSettings';
 
 app.initializers.add('blomstra/oauth-apple', () => {
-  app.extensionData.for('blomstra-oauth-apple').registerPage(AppleOAuthPage);
+  app.extensionData.for('blomstra-oauth-apple').registerPage(ConfigureWithOAuthPage);
 
   ExtendOAuthSettings();
 });
